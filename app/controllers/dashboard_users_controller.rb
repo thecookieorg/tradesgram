393ad2c8f01309706c02a29f43959e5880736a23
Marko Manojlovic <marko@thecookieapps.com>
2016-02-16 03:06:39 +0000

    Modified courses controller, and all views

--- app/controllers/dashboard_users_controller.rb
+++ app/controllers/dashboard_users_controller.rb
@@ -2,5 +2,6 @@ class DashboardUsersController < ApplicationController
   def index
     @certifications = current_user.certifications
     @experiences = current_user.experiences
+    @courses = current_user.courses
   end
 end
